843dc63afcf2846c996d313148f863a941817c54
Capries <grg06ltd@gmail.com>
2024-01-25 16:57:15 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urun1Adi = "Elma";
+            double fiyati1 = 15;
+            string aciklama1 = "Amasya elması";
+
+            string[] meyveler = new string[] {"Elma" , "Karpuz" };
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1, urun2 };
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("----------");
+
+            }
+
+            Console.WriteLine("-------Metotlar------");
+
+            //instance - örnek
+            //encapsulation
+
+            SepetMenager sepetMenager = new SepetMenager();
+            sepetMenager.Ekle(urun1);
+            sepetMenager.Ekle(urun2);
+
+            sepetMenager.Ekle2("Armut", "Yelşk armut", 12, 10);
+            sepetMenager.Ekle2("Elma", "Yelşk elma", 3, 9);
+            sepetMenager.Ekle2("limon", "sarı limon", 50, 1);
+
+        }
+    }
+}
